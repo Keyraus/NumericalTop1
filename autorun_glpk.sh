26f0cc9c89323af22ad6c9ddf104a884ad8899b4
cmarty23 <cmarty23@student.foi.hr>
2023-11-26 16:50:53 +0100

    trying multiproccessing the autorun

--- autorun_glpk.sh
+++ autorun_glpk.sh
@@ -4,6 +4,9 @@ do
     echo "Running glpsol on $file"
     # Create an output file based on the input file name and stored in ./Instances/small-lp/solutions
     output_file="./Instances/small-lp/solutions/$(basename $file .lp).out"
+    log_file="./Instances/small-lp/solutions/$(basename $file .lp).glplog"
     # Run glpsol on the input file and store the output in the output file
-    glpsol --lp $file -o $output_file
+    glpsol --lp $file -o $output_file --log $log_file &
 done
+wait
+# 
