f1f4ed13cb909efe5ed4f679a0896cb307a20f13
cmarty23 <cmarty23@student.foi.hr>
2023-11-26 18:22:38 +0100

    try smth

--- autorun_glpk.sh
+++ autorun_glpk.sh
@@ -1,12 +1,22 @@
 # For each file in ./Instances/small-lp, run the glpsol solver
-for file in ./Instances/small-lp/*.lp
+fList=()
+outList=()
+logList=()
+for dir in "$@"
 do
-    echo "Running glpsol on $file"
-    # Create an output file based on the input file name and stored in ./Instances/small-lp/solutions
-    output_file="./Instances/small-lp/solutions/$(basename $file .lp).out"
-    log_file="./Instances/small-lp/solutions/$(basename $file .lp).glplog"
-    # Run glpsol on the input file and store the output in the output file
-    glpsol --lp $file -o $output_file --log $log_file &
+    mkdir "$dir"/solutions
+    for file in "$dir"/*.lp
+    do
+        fList+=(file)
+        #echo "Running glpsol on $file"
+        # Create an output file based on the input file name and stored in ./Instances/small-lp/solutions
+        outList+=("./Instances/small-lp/solutions/$(basename $file .lp).out")
+        logList+=("./Instances/small-lp/solutions/$(basename $file .lp).glplog")
+        # Run glpsol on the input file and store the output in the output file
+        #glpsol --lp $file -o $output_file.out --log $log_file.glplog > /dev/null &
+
+        
+    done
 done
+parallel glpsol --lp {1} -o {2} --log {3} > /dev/null ::: "${fList[@]}" :::+ "${outList[@]}" :::+ "${logList[@]}"
 wait
-# 
