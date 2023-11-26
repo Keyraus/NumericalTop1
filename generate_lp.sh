86c2abdf11d588f11416b9a7d44cb4e334dcc27e
cmarty23 <cmarty23@student.foi.hr>
2023-11-26 19:45:50 +0100

    add a script for creating lp files with rep

---
+++ generate_lp.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Check for arguments
+if [ $# -eq 0 ]
+  then
+    echo "Usage : ${0} [Rep1] <Rep2> ..."
+    exit 1
+fi
+
+# Declare empty list for files name
+fList=() # List of files to proceed
+outList=() # List of solutions files to creates
+
+# Iterate threw all repertory supplied
+for dir in "$@"
+do
+    # Iterates threw all files in the repertory
+    for file in "$dir"/*.txt
+    do
+        fList+=(${file})
+        outList+=("$dir/solutions/$(basename $file .txt).lp")
+    done
+done
+
+# Run the python code in parallel using all vCPU (use -j to set a maximum count of vCPU)
+# This command is more optimized if there is more process than core on the machine
+parallel python3 ./convertion.py {1} {2} ::: "${fList[@]}" :::+ "${outList[@]}"
+
