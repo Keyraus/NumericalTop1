16f51bba70f87b44e9fae50e48cb5bf0dcccc95d
cmarty23 <cmarty23@student.foi.hr>
2023-11-26 19:21:57 +0100

    add necessary comments

--- autorun_glpk.sh
+++ autorun_glpk.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
-# For each file in ./Instances/small-lp, run the glpsol solver
-fList=()
-outList=()
-logList=()
+
+# Check for arguments
+if [ $# -eq 0 ]
+  then
+    echo "Usage : ${0} [Rep1] <Rep2> ..."
+    exit 1
+fi
+
+# Declare empty list for files name
+fList=() # List of files to proceed
+outList=() # List of solutions files to creates
+logList=() # List of logs files
+
+# Iterate threw all repertory supplied
 for dir in "$@"
 do
+    # Check and create repertory if it does not exist
     mkdir -p "$dir"/solutions
+
+    # Iterates threw all files in the repertory
     for file in "$dir"/*.lp
     do
         fList+=(${file})
-        #echo "Running glpsol on $file"
-        # Create an output file based on the input file name and stored in ./Instances/small-lp/solutions
         outList+=("./Instances/small-lp/solutions/$(basename $file .lp).out")
         logList+=("./Instances/small-lp/solutions/$(basename $file .lp).glplog")
-        # Run glpsol on the input file and store the output in the output file
-        #glpsol --lp $file -o $output_file.out --log $log_file.glplog > /dev/null &
-
-
     done
 done
 
+# Run the solver in parallel using all vCPU (use -j to set a maximum count of vCPU)
+# This command is more optimized if there is more process than core on the machine
 parallel glpsol --lp {1} -o {2} --log {3} > /dev/null ::: "${fList[@]}" :::+ "${outList[@]}" :::+ "${logList[@]}"
 
