b4aa06851fc2ea534dff08863d27709d283727da
Suna24 <bastien.t2002@gmail.com>
2023-11-28 18:19:37 +0100

    Optimize a bit the bron-kerbosh

--- main.py
+++ main.py
@@ -39,12 +39,18 @@ def main():
         #score2 = ag.intialisation_population(parse(sys.argv[1]), 1000)
         #print("Score2 : %d" % np.mean(score2))
         
-        all_clique = glouton.find_all_cliques(possible_node_to_append_to_the_clique=parse(sys.argv[1]))
-        print("Nombre de clique : %d" % len(all_clique))
-        best_clique = max(all_clique, key=lambda x: sum([i.weight for i in x]))
-        print([person.id for person in best_clique])
-        score = sum([i.weight for i in best_clique])
+        # all_clique = glouton.find_all_cliques(possible_node_to_append_to_the_clique=parse(sys.argv[1]))
+        # print("Nombre de clique : %d" % len(all_clique))
+        # best_clique = max(all_clique, key=lambda x: sum([i.weight for i in x]))
+        # print([person.id for person in best_clique])
+        # score = sum([i.weight for i in best_clique])
+        # print("Poids de la clique : %d" % score)
+
+        max_clique = glouton.find_maximum_clique(possible_node_to_append_to_the_clique=parse(sys.argv[1]))
+        print([person.id for person in max_clique])
+        score = sum([i.weight for i in max_clique])
         print("Poids de la clique : %d" % score)
+
     elif chosenOption == "4":
         score = glouton.glouton(parse(sys.argv[1]))
         print("Score : %d" % score)
