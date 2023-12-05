b6c03fc3a1539c22aea9fa314eb42c477d3097b5
Antonin Aubert <36886471+Grizfreak@users.noreply.github.com>
2023-12-05 09:30:42 +0100

    commit test

--- main.py
+++ main.py
@@ -15,15 +15,13 @@ def main():
 
     print("Resulat attendu", result)
 
-    stest = glouton.gloutonVTest(dict, 1)
+    stest = glouton.gloutonV2(dict, 0, 1)
     print(np.nonzero(stest)[0])
     #print all the raltions of 15 with their wieght
-    exit()
-    print("Score glouton : %d" % score )
-    exit()
-    gap = (int(result) - score) / int(result)
-    print("Score glouton : %d" % score )
-    print("Gap : %f" % gap)
+    #print("Score glouton : %d" % score )
+    #gap = (int(result) - score) / int(result)
+    #print("Score glouton : %d" % score )
+    #print("Gap : %f" % gap)
 
 
     score = ag.ag(dict, 0.8, 0.2, 400, 100, 500)
