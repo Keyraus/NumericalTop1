a9d6acda73ad810772d4316a87d9acef18c300cd
Suna24 <bastien.t2002@gmail.com>
2023-12-14 10:55:51 +0100

    merge conflicts

--- ant_colony_optimization.py
+++ ant_colony_optimization.py
@@ -22,12 +22,12 @@ import random
 import numpy as np
 import time
 
-pheromone_max = 10.0
+pheromone_max = 20.0
 pheromone_min = 0.1
 pheromone_update = 0.1
 pheromone_evaporation = 0.9
-nb_ants = 30
-cycle_max = 1000
+nb_ants = 100
+cycle_max = 10000
 
 
 def init_pheromones(personnes):
@@ -53,7 +53,7 @@ def select_vertex_based_probabilities(personnes, candidates, pheromones):
 def update_pheromones(pheromones, clique):
     for vertex in clique:
         pheromones[vertex.id] *= pheromone_evaporation
-        pheromones[vertex.id] += pheromone_update
+        pheromones[vertex.id] += pheromone_update * vertex.weight * len(np.nonzero(vertex.relations))
         if pheromones[vertex.id] > pheromone_max:
             pheromones[vertex.id] = pheromone_max
         elif pheromones[vertex.id] < pheromone_min:
@@ -67,7 +67,7 @@ def aoc(personnes):
     best_clique = []
     best_clique_score = 0
     # print(pheromones, len(pheromones))
-    for i in range(cycle_max):
+    for j in range(cycle_max):
         for ant in range(nb_ants):
             clique = []
             person = select_random_vertex(personnes)
@@ -92,12 +92,13 @@ def aoc(personnes):
 
             new_score = sum([vertex.weight for vertex in clique])
             if new_score > best_clique_score:
+                print(time.time() - start)
+                print("New Best Score found : ", new_score, "Itération n°", j, " => ", [p.id for p in clique])
                 best_clique_score = new_score
                 best_clique = clique
-                if new_score == 73:
-                    return best_clique, best_clique_score
 
-            if time.time() - start > 58:
+            if time.time() - start > 120:
+                # print(pheromones)
                 return best_clique, best_clique_score
 
     return best_clique, best_clique_score
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 import glouton
 import sys
 import time
@@ -26,69 +28,22 @@ def main():
     actualTime = time.time()
     # score = glouton.glouton(parse(sys.argv[1]))
     # print("Score : %d" % score)
-    clique, score2 = aoc.aoc(parse(sys.argv[1]))
-    print("Score AOC : %d" % score2)
-    # Print the result
-    dict = parse(sys.argv[1])
-    print("Temps parse : ", time.time() - start)
-    print("Resulat attendu", result)
-    start = time.time()
-    score = glouton.gloutonV2(dict)
-    end = time.time()
-    print("Score glouton : %d" % score, "Time : %.20f" % (end - start) )
+    scores = []
+    for i in range(5):
+        clique, score2 = aoc.aoc(parse(sys.argv[1]))
+        scores.append(score2)
+        print("Score AOC : %d" % score2)
+        # print the gap
+        print("Gap : %f" % ((int(result) - score2) / int(result)))
+        print("Temps d'exécution : %f" % (time.time() - actualTime))
+
+    print("Minimum : ", np.min(scores))
+    print("Moyenne : ", np.mean(scores))
+    print("Maximum : ", np.max(scores))
+
+    score = ag.ag(dict, 0.8, 2/len(dict), 400, 100, 500)
+    print("Score AG: %d" % score )
     print("Gap : %.2f" % ((int(result) - score) / int(result)))
-    for _ in range(5):
-    chosenOption = input("Choisissez l'option :\n1. Glouton\n2. Algo génétique\n3. Bron-Kerbosch\n4. Tous\n")
-    print("Option choisie : %s" % chosenOption)
-    print("Résultat de l'instance GLPK : %s" % result)
-    actualTime = time.time()
-    if chosenOption == "1":
-        score = glouton.glouton(parse(sys.argv[1]))
-        print("Score : %d" % score)
-    elif chosenOption == "2":
-        print("ToDo")
-    elif chosenOption == "3":
-        ####################################################################################
-        # Utilisation de l'algo de bron-kerbosch
-        # all_clique = glouton.find_all_cliques(possible_node_to_append=parse(sys.argv[1]))
-        # print("Nombre de clique : %d" % len(all_clique))
-        # best_clique = max(all_clique, key=lambda x: sum([i.weight for i in x]))
-        # print(person.id for person in best_clique)
-        # print("Poids de la clique : %d" % sum([i.weight for i in best_clique]))
-        #score2 = ag.intialisation_population(parse(sys.argv[1]), 1000)
-        #print("Score2 : %d" % np.mean(score2))
-        
-        # all_clique = glouton.find_all_cliques(possible_node_to_append_to_the_clique=parse(sys.argv[1]))
-        # print("Nombre de clique : %d" % len(all_clique))
-        # best_clique = max(all_clique, key=lambda x: sum([i.weight for i in x]))
-        # print([person.id for person in best_clique])
-        # score = sum([i.weight for i in best_clique])
-        # print("Poids de la clique : %d" % score)
-
-        max_clique = glouton.find_maximum_clique(possible_node_to_append_to_the_clique=parse(sys.argv[1]))
-        print([person.id for person in max_clique])
-        score = sum([i.weight for i in max_clique])
-        print("Poids de la clique : %d" % score)
-
-    elif chosenOption == "4":
-        score = glouton.glouton(parse(sys.argv[1]))
-        print("Score : %d" % score)
-        score2 = ag.intialisation_population(parse(sys.argv[1]), 1000)
-        print("Score2 : %d" % np.mean(score2))
-        all_clique = glouton.find_all_cliques(possible_node_to_append=parse(sys.argv[1]))
-        print("Nombre de clique : %d" % len(all_clique))
-        best_clique = max(all_clique, key=lambda x: sum([i.weight for i in x]))
-        print(person.id for person in best_clique)
-        print("Poids de la clique : %d" % sum([i.weight for i in best_clique]))
-    else:
-        print("Option invalide")
-    # print the gap
-    print("Gap : %f" % ((int(result) - score2) / int(result)))
-    print("Temps d'exécution : %f" % (time.time() - actualTime))
-
-        score = ag.ag(dict, 0.8, 2/len(dict), 400, 100, 500)
-        print("Score AG: %d" % score )
-        print("Gap : %.2f" % ((int(result) - score) / int(result)))
     
 if __name__ == "__main__":
     main()
